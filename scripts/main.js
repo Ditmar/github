14410c9e7de988484cf4ae19c381ac997736e122
Desktop <spyatorio@gmail.com>
2021-08-27 16:20:02 -0400

    new code

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,26 @@
 import ManagementProducts from "./MagementProducts.js";
 const main = () => {
-  let [addProducts, removeProducts, updateProducts, getProducts] =
+  const [addProducts, removeProducts, updateProducts, getProducts] =
     ManagementProducts();
-  const products = getProducts();
+
   console.log(getProducts());
   let tagList = document.getElementById("list");
-  let result = products.reduce((html, items) => {
-    html = html + `<li>${items}</li>`;
-    return html;
-  }, "");
-  console.log(result);
-  tagList.innerHTML = result;
+  let inputDom = document.getElementById("inputtxt");
+  let form = document.getElementById("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addProducts(inputDom.value);
+    render();
+    inputDom.value = "";
+  });
+  const render = () => {
+    const products = getProducts();
+    let result = products.reduce((html, items) => {
+      html = html + `<li>${items}</li>`;
+      return html;
+    }, "");
+    tagList.innerHTML = result;
+  };
+  render();
 };
 main();
