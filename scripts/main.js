9476b906e147f1d10df3ae594289b5500ad489d6
Ditmaros <ditmarcastro@gmail.com>
2021-08-27 16:17:28 -0400

    ok

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,21 @@ import ManagementProducts from "./MagementProducts.js";
 const main = () => {
   let [addProducts, removeProducts, updateProducts, getProducts] =
     ManagementProducts();
+  const reduce = () => {
+    return "WHOPS";
+  };
+  let i = 67;
+  for (let i = 0; i < 34; i++) {}
+  for (let i = 0; i < 34; i++) {}
+  for (let i = 0; i < 34; i++) {}
+  for (let i = 0; i < 34; i++) {}
+  for (let i = 0; i < 34; i++) {}
+  for (let i = 0; i < 34; i++) {}
+
   const products = getProducts();
   console.log(getProducts());
   let tagList = document.getElementById("list");
+  let a = "hola mundo";
   let result = products.reduce((html, items) => {
     html = html + `<li>${items}</li>`;
     return html;
